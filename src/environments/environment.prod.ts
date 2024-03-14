19eda41c384809112c7c2a13ac154a0b3a07c963
k.hilarion <k.hilarion@easytech-africa.com>
2024-03-14 09:26:29 +0100

    Definition of environments - local and production

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: true,
+    Base_API_URL: 'https://starter-api.hachesolutions.com',
+    Site_URL: 'https://aspnetcore-starter-kit.hachesolutions.com',
+    Expiration_Time: 60
+};
